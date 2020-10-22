b55f0e8c125f429ddb3a668bd918f7f0cce7fdb5
Sowiriro <sowiriro@gmail.com>
2020-10-22 11:03:39 +0900

    add CMIS141 Week1

---
+++ CMIS141/Week1/HelloUMUC.java
@@ -0,0 +1,7 @@
+
+public class HelloUMUC {
+    public static void main(String[] args) {
+            //Display the string.s
+            System.out.println("Hello UMUC! My name is Soichiro Ono. Nice to see you! ");
+    }
+}
---
+++ CMIS141/Week1/Week1Quiz.java
@@ -0,0 +1,40 @@
+// public class HelloUMUC {
+//     public static void main(String[] args) {
+//             //Display the string.s
+//             String var1 = hello;
+//             String var2 = umuc;
+//             System.out.println(var1   +     var2);
+
+//     }
+// }
+
+// public class QuizItem
+// {
+//    public static void main(String[] args)
+//      {    
+//      final int age = 38;
+//      age = 39;
+//       System.out.println("Age is " + age);
+//    }
+// }
+
+
+
+// public class Week1Quiz {
+
+//     public static void main (String[] args) {
+ 
+//       System.out.print ("Welcome");
+ 
+//       System.out.print ("to ");
+ 
+//       System.out.println ("UMUC");
+ 
+//  System.out.print ("!");          
+ 
+//     }
+ 
+//  }
+ 
+ 
+
