ee44b5ceab92424733adf5217c4b55860d91d954
Sowiriro <sowiriro@gmail.com>
2020-11-05 14:57:46 +0900

    add Week3 question.java

---
+++ CMIS141/Week3/Question.java
@@ -0,0 +1,132 @@
+public class Question
+ {
+
+  //quiz 25 
+  // public static void main(String args[]){            
+  //   String string1 = "243";        
+  //   System.out.println(Integer.parseInt(string1));
+  //  }
+
+  //quiz 24 
+  // public static void main(String args[]){  
+  //   int cnt = 0;
+  //   for (int rowCnt=0; rowCnt <= 4; rowCnt++) {      
+  //     for (int colCnt=0; colCnt<= 4; colCnt++) {
+  //       cnt++;                 
+  //      }
+  //   } 
+  //   System.out.println(cnt);
+  //  }
+
+  //quiz 23 user
+//   public static void main(String args[]){  
+//     int colCnt = 0;
+//     int rowCnt = 0;
+//  do  {       
+//     colCnt = 0;        
+//    do  {
+//       System.out.print("hi" + "\t");            
+//           colCnt++;
+//     } while (colCnt < 8);           
+//      System.out.print("\n");
+//          rowCnt++;
+//      } while (rowCnt < 7);
+// }
+
+
+  //quiz 22 user
+  // public static void main (String args[]) {
+  //   int i = 0;
+  //   for (i = 0; i < 10; i++);
+  //     System.out.println(i + 4);
+  // }
+
+  //quiz 21 -
+//   public static void main(String args[]){
+//   int count = 0;
+// do {
+//    System.out.println("Welcome to Java");
+//    count++;
+// } while (count < 10);
+//   }
+
+  // quiz 19 
+//   public static void main(String args[]){
+//     double examAvg = 85.9983;      
+//     System.out.println(Math.ceil(examAvg));     
+// }
+
+  //  quiz  16
+  // public static void main(String args[]){      
+  //       double angle = 90;
+  //       angle = Math.toRadians(angle);   
+  //       System.out.println(Math.sin(angle) + Math.pow(Math.cos(angle),2));      
+  // }
+
+
+  // quiz  15
+//   public static void main(String args[]){
+//     int balance = 10;
+// while (balance >= 1) {
+//   if (balance < 9) {
+//     break;
+//               }
+//   balance = balance - 9;
+// }
+// System.out.println(balance);
+//   }
+
+  // quiz 13 
+  // public static void main(String args[]){            
+  //   String string1 = "CMIS141 ";
+  //   String string2 = "is all";
+  //   String string3 = "about Java!";
+  //   String concat = string3.concat(string2).concat(string1);    
+  //   System.out.println(concat.length());
+  //  }
+
+      //   public static void main(String args[]){  
+      //     int cnt = 1;
+      //     int maxLoop = 30;          
+      //     while (cnt < maxLoop) {              
+      //             cnt++;
+      //         }
+      // }
+    // Question 8 
+    // public static void main(String args[]){       
+    //     int momAge = 62;
+    //     int dadAge = 64;      
+    //     System.out.println(Math.max(momAge, dadAge) + Math.min(momAge, dadAge));
+    //  }
+
+    //quiz 6  
+//     public static void main(String args[]){
+//       int x = 0;
+// int y = 0;  
+// switch (x + 1)  {
+//   case 0: y = 0;
+//   case 1: y = 1;
+//   default: y = -1;
+// }
+// System.out.println(y);
+//     }
+
+//quiz 3 user
+public static void main(String args[]){
+int y = 0;
+for (int i = 0; i<10; ++i) {
+   y += i;
+}
+System.out.println(y);
+}
+
+//quiz 1 
+// public static void main(String args[]){            
+//   String string1 = "243";    
+//   String string2 = "true";
+//   String string3 = "24.1"    ;
+//   boolean results = (Boolean.parseBoolean(string2) && false);
+//   System.out.println(results);
+//  }
+
+}
